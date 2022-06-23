8ef478e428684077d00e8346931aba817b06ab1c
Guix <88253846+xGuix@users.noreply.github.com>
2022-06-23 13:08:25 +0200

    Last UpDate Clean up code

---
+++ RewardCentral/src/test/java/RewardCentral/controller/RewardCentralControllerTest.java
@@ -0,0 +1,11 @@
+package RewardCentral.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(RewardCentralController.class)
+public class RewardCentralControllerTest {
+
+}
